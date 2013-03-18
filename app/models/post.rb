d92da8b761e41d444e864737924a6201fa937386
丁历祥 <dlx_swpu@qq.com>
2013-03-18 20:48:37 +0800

    ads

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ActiveRecord::Base
+  attr_accessible :content, :title
+
+  validates :title,:presence => true,:uniqueness => true
+  validates :content,:presence => true
+
+  has_many :comments
+end
